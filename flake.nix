67ece235445924d1cba503a574db5f19427b2720
ImToLate <Fritzipweber@icloud.com>
2026-06-12 10:46:20 +0200

    added Mindustry flake

--- flake.nix
+++ flake.nix
@@ -1,14 +1,25 @@
-y {
+{
   description = "Mindustry flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { nixpkgs, ... }: {
-    pkgs = nixpkgs.legacyPackages;
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system}.default = pkgs.mindustry;
 
-    packages.default = pkgs.mindustry;
-  };
+      apps.${system}.default = {
+        type = "app";
+        program = "${pkgs.mindustry}/bin/mindustry";
+
+        meta.description = "Launch Mindustry";
+      };
+    };
 
 }
